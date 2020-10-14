27e54045ead8b3b09b397c8ec9b85699cd65e966
Verona <nebesnav@gmail.com>
2020-10-14 21:38:41 +0300

    add read more btn

--- src/app/Main/Blog/BlogListItem.js
+++ src/app/Main/Blog/BlogListItem.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react"
-
+import ReadMoreBtn from "./ReadMoreBtn"
 const BlogListItem= ({
     id,
     img,
@@ -22,7 +22,7 @@ const BlogListItem= ({
                         <p className="subtitle_blog_item">{subtitle_blog_item}</p>
                         <p className="date_of_post">{date_of_post}</p>
                         <p className="desc_of_posts">{desc_of_posts}</p>
-                        <p className="btn_read_more">Read more <i class="fas fa-long-arrow-alt-right"></i></p>
+                       <ReadMoreBtn/>
                     </div>
                 </div>
             </div>
---
+++ src/app/Main/Blog/ReadMoreBtn.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react"
+
+const ReadMoreBtn =()=>{
+    return(
+        <Fragment>
+            <p className="btn_read_more">Read more <i class="fas fa-long-arrow-alt-right"></i></p>
+        </Fragment>
+    )
+}
+
+export default ReadMoreBtn
