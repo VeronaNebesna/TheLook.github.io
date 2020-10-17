44e506b66a09547701770f21cac2d40dac31a261
Verona <nebesnav@gmail.com>
2020-10-17 20:36:00 +0300

    added lightbox in gallery view

--- src/app/Main/Main.js
+++ src/app/Main/Main.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment,useState, useCallback  } from "react";
 import {Route} from "react-router-dom";
 import Gallery from "react-photo-gallery";
 import PhotoListLeft from "./PhotoList/PhotoListLeft";
@@ -10,18 +10,46 @@ import {photos} from "./photos";
 import ScrollToTopOnMount from "./ScrollToTopMount";
 import GalleryLink from "../Components/Gallery/GalleryLink";
 import "./Main.css";
+import Carousel, { Modal, ModalGateway } from "react-images";
+
 
 
 
 const Main = () =>{
+    const [currentImage, setCurrentImage] = useState(0);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false);
+  
+    const openLightbox = useCallback((event, { photo, index }) => {
+      setCurrentImage(index);
+      setViewerIsOpen(true);
+    }, []);
+  
+    const closeLightbox = () => {
+      setCurrentImage(0);
+      setViewerIsOpen(false);
+    };
     return(
         <Fragment>
             <ScrollToTopOnMount/>
              <Route path ="/gallery" exact component={GalleryLink}/>
              <Route path ="/gallery_view" exact render={()=>(
-                   <Gallery photos={photos} direction={"column"}/>
+                   <Gallery photos={photos} direction={"column"}
+                   onClick={openLightbox}/>
              )}/>
-
+<ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map(x => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
             <section className="section">
                 <div className="container">
                     <div className ="row section_row">
--- src/app/Main/photos.js
+++ src/app/Main/photos.js
@@ -2,67 +2,80 @@ export const photos=[
     {
         src: "/images/gallery_view/1.jpg",
         width: 4,
-        height: 3
+        height: 3,
+        alt:"Photo 1"
       },
       {
         src: "/images/gallery_view/2.jpg",
         width: 3,
-        height: 3
+        height: 3,
+        alt:"Photo 2"
       },
       {
         src: "/images/gallery_view/3.jpg",
         width: 3,
-        height: 4
+        height: 4,
+        alt:"Photo 3"
       },
       {
         src:"/images/gallery_view/4.jpg",
         width: 3,
-        height: 4
+        height: 4,
+        alt:"Photo 4"
       },
       {
         src: "/images/gallery_view/5.jpg",
         width: 5,
-        height: 4
+        height: 4,
+        alt:"Photo 5"
       },
       {
         src: "/images/gallery_view/6.jpg",
         width: 4,
-        height: 3
+        height: 3,
+        alt:"Photo 6"
       },
       {
         src: "/images/gallery_view/7.jpg",
         width: 3,
-        height: 4
+        height: 4,
+        alt:"Photo 7"
       },
       {
         src: "/images/gallery_view/8.jpg",
         width: 5,
-        height: 5
+        height: 5,
+        alt:"Photo 8"
       },
       {
         src: "/images/gallery_view/9.jpg",
         width: 3,
-        height:3
+        height:3,
+        alt:"Photo 9"
       },
       {
         src: "/images/gallery_view/10.jpg",
         width: 4,
-        height: 5
+        height: 5,
+        alt:"Photo 10"
       },
       {
         src: "/images/gallery_view/11.jpg",
         width: 4,
-        height: 5
+        height: 5,
+        alt:"Photo 11"
       },
       {
         src: "/images/gallery_view/12.jpg",
         width: 4,
-        height: 6
+        height: 6,
+        alt:"Photo 12"
       },
       {
         src: "/images/gallery_view/13.jpg",
         width: 4,
-        height: 3
+        height: 3,
+        alt:"Photo 13"
       }
 ]
 
